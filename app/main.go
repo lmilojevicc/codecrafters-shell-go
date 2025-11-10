8405ce51b0c44b5592e19014dff9ade222f3a9d7
Luka <104729643+lmilojevicc@users.noreply.github.com>
2025-11-10 14:40:49 +0100

    Finish Implement REPL

--- app/main.go
+++ app/main.go
@@ -4,15 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "couldn't read input: %v\n", err)
-		os.Exit(1)
-	}
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	fmt.Fprintln(os.Stdout, command[:len(command)-1]+": not found")
+	go func() {
+		s := <-sigs
+		fmt.Fprintf(os.Stdout, "received signal %s, exiting\n", s)
+		os.Exit(0)
+	}()
+
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "couldn't read input: %v\n", err)
+			os.Exit(1)
+		}
+
+		command = strings.TrimRight(command, "\r\n")
+		fmt.Fprintln(os.Stdout, command+": not found")
+	}
 }
