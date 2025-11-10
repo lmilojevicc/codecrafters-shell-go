2de4ad2827566079ce8c3f6a4a7ca5328bd357b1
Luka <104729643+lmilojevicc@users.noreply.github.com>
2025-11-10 14:58:46 +0100

    Implement exit

--- app/main.go
+++ app/main.go
@@ -21,13 +21,33 @@ func main() {
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		commandStr, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "couldn't read input: %v\n", err)
 			os.Exit(1)
 		}
 
-		command = strings.TrimRight(command, "\r\n")
+		commandStr = strings.TrimRight(commandStr, "\r\n")
+		commandWithArgs := strings.Split(commandStr, " ")
+
+		command := commandWithArgs[0]
+		commandArgs := commandWithArgs[1:]
+
+		switch command {
+		case "exit":
+			if len(commandArgs) < 1 {
+				fmt.Println("exit command takes one argument")
+				continue
+			}
+
+			switch commandArgs[0] {
+			case "0":
+				os.Exit(0)
+			case "1":
+				os.Exit(1)
+			}
+		}
+
 		fmt.Fprintln(os.Stdout, command+": not found")
 	}
 }
